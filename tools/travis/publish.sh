3880c5412504fa9c5a411f680df41004d1570249
Brendan McAdams <brendan@bytes.codes>
2018-08-21 11:59:50 -0700

    fix check for docker credentials (matches java runtime)

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -13,7 +13,8 @@ cd $ROOTDIR
 COMMIT=$(git rev-parse HEAD | cut -c 1-7)
 VERSION=${1:-$COMMIT}
 
-if [ "${VERSION}" != "openshift-latest" ]; then
+
+if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
   docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 fi
 
